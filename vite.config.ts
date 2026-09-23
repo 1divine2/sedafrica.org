650ca858404609340b99265796b53f02a6f436a5
Solomon Abdul Divine Kamara <144345554+1divine2@users.noreply.github.com>
2026-09-23 15:51:43 +0000

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,20 @@ import { defineConfig } from "vite";
 import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
 
-const root = import.meta.dirname;
-
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        main: resolve(root, "index.html"),
-        admin: resolve(root, "admin.html"),
-        education: resolve(root, "education.html"),
-        agriculture: resolve(root, "agriculture.html"),
-        genderChildren: resolve(root, "gender-children.html"),
-        communityEmpowerment: resolve(root, "community-empowerment.html"),
-        privacyPolicy: resolve(root, "privacy-policy.html"),
-        termsConditions: resolve(root, "terms-conditions.html"),
-        cookiePolicy: resolve(root, "cookie-policy.html"),
+        main: resolve("index.html"),
+        admin: resolve("admin.html"),
+        education: resolve("education.html"),
+        agriculture: resolve("agriculture.html"),
+        genderChildren: resolve("gender-children.html"),
+        communityEmpowerment: resolve("community-empowerment.html"),
+        privacyPolicy: resolve("privacy-policy.html"),
+        termsConditions: resolve("terms-conditions.html"),
+        cookiePolicy: resolve("cookie-policy.html"),
       },
     },
   },
